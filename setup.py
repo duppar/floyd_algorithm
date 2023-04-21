8bfbbb4aa8af98392303d3c0659000b222d2614b
duppar <sgrduppa@liverpool.ac.uk>
2023-04-21 10:15:04 +0100

    Packing files for shorteat_path

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shortest_path",
+    packages=find_packages(),
+    version="0.1.0",
+    description="floyd recursion shortest path",
+    author="Rachael Duppa-Miller",
+    license="MIT",
+    install_requires=[],
+)
