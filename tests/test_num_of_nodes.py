eb6d688c1deac9c9b16cfe0f9c87e9abf3eae93b
duppar <129500210+duppar@users.noreply.github.com>
2023-04-25 12:03:51 +0100

    Update test_num_of_nodes.py
    removing user ids

--- tests/test_num_of_nodes.py
+++ tests/test_num_of_nodes.py
@@ -5,7 +5,7 @@ import sys
 import unittest
 
 sys.path.append(
-    "C:\\Users\\610109025\\OneDrive - BT Plc\\Documents - RDM Admin\\General\\Post Grad\\sw developement\\floyd_algorithm\\code"
+    "C:\\Users\\xxxxxxxxx\\OneDrive - BT Plc\\Documents - RDM Admin\\General\\Post Grad\\sw developement\\floyd_algorithm\\code"
 )
 
 from shortest_path import num_of_nodes
